1c9576073a01b5364678005847b2e008ae7eae70
JoãoPedroDALLE <64556526+JoaoPedroDALLE@users.noreply.github.com>
2021-07-07 18:12:08 -0300

    Add files via upload

---
+++ AvaliacaoWebMeDa5AiSor/includes/footer.php
@@ -0,0 +1,27 @@
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
+      <script>
+      	 M.AutoInit();
+      </script>
+
+      <div class="footer-copyright deep-purple lighten-3" style="
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 10%;
+        background-color: red;
+        color: white;
+        text-align: center;">
+          <div class="container">
+            De João Pedro Dalle da Silva
+            <ul class="right hide-on-med-and-down">
+              <li><a href="https://gmail.com"><i class="material-icons">email</i></a></li>
+              <li><a href="https://github.com/JoaoPedroDALLE"><i class="material-icons">group</i></a></li>
+            </ul>
+          </div>
+      </div>
+
+      
+    </body>
+  </html>
